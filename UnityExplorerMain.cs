3b4a3b33cdf9ba530bb13245e4305b5561010732
zrydnoob <2642935611@qq.com>
2024-08-26 13:53:44 +0800

    添加项目文件。

---
+++ UnityExplorerMain.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using RWCustom;
+using UnityExplorer;
+using BepInEx;
+using Debug = UnityEngine.Debug;
+
+
+#pragma warning disable CS0618
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace TemplateMod;
+
+[BepInPlugin("zrydnoob.UnityExplorer", "Unity Explorer", "1.0.0")]
+public partial class UnityExplorerMain : BaseUnityPlugin
+{
+
+    private void OnEnable()
+    {
+        On.RainWorld.OnModsInit += RainWorldOnOnModsInit;
+    }
+
+    private bool IsInit;
+    private void RainWorldOnOnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+        try
+        {
+            if (IsInit) return;
+
+
+            On.RainWorldGame.ShutDownProcess += RainWorldGameOnShutDownProcess;
+            On.GameSession.ctor += GameSessionOnctor;
+
+            UnityExplorer.ExplorerStandalone.CreateInstance();
+
+            IsInit = true;
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex);
+            throw;
+        }
+    }
+    
+    private void RainWorldGameOnShutDownProcess(On.RainWorldGame.orig_ShutDownProcess orig, RainWorldGame self)
+    {
+        orig(self);
+        ClearMemory();
+    }
+    private void GameSessionOnctor(On.GameSession.orig_ctor orig, GameSession self, RainWorldGame game)
+    {
+        orig(self, game);
+        ClearMemory();
+    }
+
+    #region Helper Methods
+
+    private void ClearMemory()
+    {
+
+    }
+
+    #endregion
+}
